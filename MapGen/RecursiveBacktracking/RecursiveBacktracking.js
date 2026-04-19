611880704a811ee4dacecff42addd9b9bd03cc1b
TheCookieLegend <lucas2.olafsen@gmail.com>
2026-04-19 17:42:11 +0200

    Result of recursive backtracker can be seen on website
    THe maze generation itself does not work yet tho. It produced a bit of an odd maze (all white in the middle)

--- MapGen/RecursiveBacktracking/RecursiveBacktracking.js
+++ MapGen/RecursiveBacktracking/RecursiveBacktracking.js
@@ -1,7 +1,5 @@
 import { maps, create2D } from "../../2dArray.js"; //import the mapsize from the js file, MapGen
 export { recursiveBacktracker };
-let size_of_mapA = 10;
-let directions = [];
 var direction;
 (function (direction) {
     direction[direction["UP"] = 0] = "UP";
@@ -21,30 +19,104 @@ function recursiveBacktracker(sizeOfMap) {
             if (i % 2 === 1 || j % 2 === 1) {
                 map[i][j] = 1; // free tile
             }
+            //mark borders as visited
             if (i === 0 || j === 0 || i === mapHeight - 1 || j === mapWidth - 1) {
                 visited[i][j] = 0.5; // visited
             }
         }
     }
+    chooseStartCell(visited);
+    convertVisitedToMap(visited, map);
     return map;
 }
-function generate(map, x, y) {
-    map[x][y] = 0.5;
-    const right = map[x][y + 2];
-    const up = map[x - 2][y];
-    const left = map[x][y - 2];
-    const down = map[x + 2][y];
+function chooseStartCell(visited) {
+    let validNumbers = [];
+    for (let x = 2; x < visited.length; x += 2) {
+        validNumbers.push(x);
+    }
+    //choose random cell
+    let indexSy = Math.floor((Math.random() * validNumbers.length));
+    let indexSx = Math.floor((Math.random() * validNumbers.length));
+    let sy = validNumbers[indexSy];
+    let sx = validNumbers[indexSx];
+    generateMazeFromCell(visited, sy, sx);
+}
+function generateMazeFromCell(visited, y, x) {
+    visited[y][x] = 0.5;
+    let up, down, left, right;
+    if (y >= 2) {
+        up = visited[y - 2][x];
+    }
+    if (y < visited.length - 3) {
+        down = visited[y + 2][x];
+    }
+    if (x >= 2) {
+        left = visited[y][x - 2];
+    }
+    if (x < visited[0].length - 3) {
+        right = visited[y][x + 2];
+    }
     if (right === 0.5 && up === 0.5 && left === 0.5 && down === 0.5) {
         //pass
     }
     else {
-        let li = [1, 2, 3, 4];
-        if (li.length === 0) {
-            console.log(undefined);
+        let li = [0, 1, 2, 3];
+        while (li.length > 0) {
+            const visitedHeight = visited.length;
+            const visitedWidth = visited[0].length;
+            const indexDir = Math.floor(Math.random() * li.length);
+            const dir = li[indexDir];
+            let nx, ny, mx, my;
+            if (li.length === 0) {
+                console.log(undefined);
+            }
+            else {
+                li.splice(indexDir, 1); //removes the element from the array
+            }
+            if (dir === direction.UP) {
+                nx = x;
+                mx = x;
+                ny = y - 2;
+                my = y - 1;
+            }
+            else if (dir === direction.DOWN) {
+                nx = x;
+                mx = x;
+                ny = y + 2;
+                my = y + 1;
+            }
+            else if (dir === direction.LEFT) {
+                nx = x - 2;
+                mx = x - 1;
+                ny = y;
+                my = y;
+            }
+            else if (dir === direction.RIGHT) {
+                nx = x + 2;
+                mx = x + 1;
+                ny = y;
+                my = y;
+            }
+            else {
+                nx = x;
+                mx = x;
+                ny = y;
+                my = y;
+            }
+            if (nx >= 0 && nx < visitedWidth && ny >= 0 && ny < visitedHeight && visited[ny][nx] !== 0.5) {
+                visited[my][mx] = 0.5;
+                generateMazeFromCell(visited, ny, nx);
+            }
         }
-        else {
-            const dir = Math.floor(Math.random() * li.length);
-            const result = li[dir];
+    }
+}
+function convertVisitedToMap(visited, map) {
+    //0.5 cells from visited array is made into 1 cells in map array 
+    for (let i = 1; i < visited.length - 1; i++) {
+        for (let j = 1; j < visited[0].length - 1; j++) {
+            if (visited[i][j] === 0.5) {
+                map[i][j] = 1;
+            }
         }
     }
 }
@@ -52,41 +124,4 @@ function generate(map, x, y) {
 //Repeat the process until there are no more adjacent cells to choose from.
 //Start backtracking until you can choose a cell again.
 //The algorithm is done when you return to the starting cell.
-//Kig på naboer, som ikke er besøgt endnu
-//Vælg en tilfældig nabo
-//“Bryd væggen” mellem dem
-//Gå videre derfra
-//Når der ikke er flere muligheder, går funktionen automatisk tilbage til forrige celle
-//Fortsæt indtil alt er besøgt
-/*
-let top_left_border: number = 0;
-let low_right_border: number = sizeofmap - 1;
-//Add a visited variable to cell object (here the object is aldready made)
-for (let i: number = 0; i < sizeofmap; i++) {
-    directions[i] = []; //create directions array
-
-    for(let j: number = 0; j < sizeofmap; j++) {
-        mapsize[i][j].visited = false; //set visited variable to false
-        directions[i][j] = {}; //create object for directions
-
-        if (i > top_left_border) {
-            directions[i][j].up = mapsize[i-1][j];
-        }
-        if (i < low_right_border) {
-            directions[i][j].down = mapsize[i+1][j];
-        }
-        if (j > top_left_border) {
-            directions[i][j].left = mapsize[i][j-1];
-        }
-        if (j < low_right_border) {
-            directions[i][j].right = mapsize[i][j+1];
-        }
-    }
-}
-startcell.visited = true;
-
-if (up && up.visited == false) {
-
-}
-*/ 
 //# sourceMappingURL=RecursiveBacktracking.js.map

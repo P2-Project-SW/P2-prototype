e806ef7261bd63448e1a44bb3c298155d73e08ad
VictoriaKraghede <vkragh22@student.aau.dk>
2026-04-23 11:27:56 +0200

    Update A-star Algorithm.ts

--- A-star Algorithm/A-star Algorithm.ts
+++ A-star Algorithm/A-star Algorithm.ts
@@ -1,4 +1,6 @@
 import { maps } from "../2dArray.js";
+import { recursiveBacktracker } from "../MapGen/RecursiveBacktracking/RecursiveBacktracking.js";
 
+let map = recursiveBacktracker(15);
 //eksempel: 
 // maps.small - henter small map

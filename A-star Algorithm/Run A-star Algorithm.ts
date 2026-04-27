612c183bc9d5a133a03d49cec3f86cc759cedd58
Sabrina1411 <sabrina.14.esk@gmail.com>
2026-04-27 11:39:21 +0200

    A* Algorithm

--- A-star Algorithm/Run A-star Algorithm.ts
+++ A-star Algorithm/Run A-star Algorithm.ts
@@ -1,7 +1,7 @@
-import { maps } from "../2dArray.js";
+/*
 import { Graph, search, heuristics } from "./A-star Algorithm.js";
 
-const grid = maps;
+//const grid;
 
 const graph = new Graph(grid.small.grid, { diagonal: false });
 const start = graph.grid[1]![1]!;
@@ -9,3 +9,4 @@ const end   = graph.grid[10]![10]!;
 
 const path = search(graph, start, end, { heuristic: heuristics.manhattan });
 console.log(path.map(n => n.toString()));
+*/

328f9d8dbacd95b433d43890ce11b737d8143cba
Heini2001 <hjoen23@student.aau.dk>
2026-05-07 12:33:17 +0200

    removed cache

--- dist/AStar/AStar.d.ts
+++
@@ -1,6 +0,0 @@
-export type Point = {
-    x: number;
-    y: number;
-};
-export declare function aStar(grid: number[][], start: Point, goal: Point): Point[];
-//# sourceMappingURL=AStar.d.ts.map
--- dist/AStar/helpers.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Point } from "./AStar.js";
-export declare function findGoal(grid: number[][]): Point | undefined;
-//# sourceMappingURL=helpers.d.ts.map
